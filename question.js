0cdd258c750287a6ddc7b5bd41d643b236d99cb0
abrahamgit <abraham@360ground.com>
2017-04-19 10:24:41 +0300

    add get by id functionality

--- question.js
+++ question.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var exams = [
-    {id: 1, title: "Question Title 1", question: "1. the question goes here", answer: "1. the answer goes here", level: 1 ,subject: "1. the question subject goes here", timeout: 60 ,icon: "image1.png", instruction: "1. the instruction for the question goes here", explanation:"the explanation 1 goes here", trial: 0 },
-    {id: 2, title: "Question Title 2", question: "2. the question goes here", answer: "2. the answer goes here", level: 2 ,subject: "2. the question subject goes here", timeout: 60 ,icon: "image2.png", instruction: "2. the instruction for the question goes here", explanation:"the explanation 2 goes here", trial: 0 },
-    {id: 3, title: "Question Title 3", question: "3. the question goes here", answer: "3. the answer goes here", level: 2 ,subject: "3. the question subject goes here", timeout: 60 ,icon: "image3.png", instruction: "3. the instruction for the question goes here", explanation:"the explanation 3 goes here", trial: 0 },
-    {id: 4, title: "Question Title 4", question: "4. the question goes here", answer: "4. the answer goes here", level: 1 ,subject: "3. the question subject goes here", timeout: 60 ,icon: "image4.png", instruction: "4. the instruction for the question goes here", explanation:"the explanation 4 goes here", trial: 0 },
-    {id: 5, title: "Question Title 5", question: "5. the question goes here", answer: "5. the answer goes here", level: 3 ,subject: "2. the question subject goes here", timeout: 60 ,icon: "image5.png", instruction: "5. the instruction for the question goes here", explanation:"the explanation 5 goes here", trial: 0 },
-    {id: 6, title: "Question Title 6", question: "6. the question goes here", answer: "6. the answer goes here", level: 3 ,subject: "1. the question subject goes here", timeout: 60 ,icon: "image6.png", instruction: "6. the instruction for the question goes here", explanation:"the explanation 6 goes here", trial: 0 },
+    {id: 1001, title: "Question Title 1", question: "1. the question goes here", answer: "1. the answer goes here", level: 1 ,subject: "1. the question subject goes here", timeout: 60 ,icon: "image1.png", instruction: "1. the instruction for the question goes here", explanation:"the explanation 1 goes here", trial: 0 },
+    {id: 1002, title: "Question Title 2", question: "2. the question goes here", answer: "2. the answer goes here", level: 2 ,subject: "2. the question subject goes here", timeout: 60 ,icon: "image2.png", instruction: "2. the instruction for the question goes here", explanation:"the explanation 2 goes here", trial: 0 },
+    {id: 1003, title: "Question Title 3", question: "3. the question goes here", answer: "3. the answer goes here", level: 2 ,subject: "3. the question subject goes here", timeout: 60 ,icon: "image3.png", instruction: "3. the instruction for the question goes here", explanation:"the explanation 3 goes here", trial: 0 },
+    {id: 1004, title: "Question Title 4", question: "4. the question goes here", answer: "4. the answer goes here", level: 1 ,subject: "3. the question subject goes here", timeout: 60 ,icon: "image4.png", instruction: "4. the instruction for the question goes here", explanation:"the explanation 4 goes here", trial: 0 },
+    {id: 1005, title: "Question Title 5", question: "5. the question goes here", answer: "5. the answer goes here", level: 3 ,subject: "2. the question subject goes here", timeout: 60 ,icon: "image5.png", instruction: "5. the instruction for the question goes here", explanation:"the explanation 5 goes here", trial: 0 },
+    {id: 1006, title: "Question Title 6", question: "6. the question goes here", answer: "6. the answer goes here", level: 3 ,subject: "1. the question subject goes here", timeout: 60 ,icon: "image6.png", instruction: "6. the instruction for the question goes here", explanation:"the explanation 6 goes here", trial: 0 },
 
 ];
 
@@ -14,6 +14,22 @@ var exams = [
 
 router.get('/', function(req, res){
     res.json(exams);
+});
+
+
+router.get('/:id([0-9]{4,})', function(req, res){
+    var currExam = exams.filter(function(exam){
+        if(exam.id == req.params.id){
+            return true;
+        }
+    });
+    if(currExam.length == 1){
+        res.json(currExam[0])
+    }
+    else{
+        res.status(404);//Set status to 404 as exam was not found
+        res.json({message: "SORRY! The Exam was Not Found"});
+    }
 });
 
 
