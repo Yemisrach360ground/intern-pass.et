39eb1d445dbc70498f346650825d672322d14fa8
abrahamgit <abraham@360ground.com>
2017-04-20 03:19:10 +0300

    updating

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var upload = multer();
 
 var app = express();
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/my_db');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(upload.array());
