974b1ad526ccb9a94e7f44451e6024fb9018a8f9
abrahamgit <abrahamgit@github.com>
2017-04-20 09:48:12 +0300

    correcting the suggestions

--- question.js
+++ question.js
@@ -12,111 +12,127 @@ var exams = [
         //todo what is trial?
         id: 1001,
         title: "Question Title 1", 
-        question: "1. the question goes here", 
+        question: "the question goes here", 
         options: [{
             optionA:"option a element",
             optionB:"option b element", 
             optionC:"option c element",
             optionD:"option d element "
         }],
-        answer: "1. the answer goes here", 
+        answer: "options.optionA", 
         level: 1,
-        subject: "1. the question subject goes here",
-        timeout: 60,
+        subject: "the question subject goes here",
+        duration: 60,
         icon: "image1.png",
-        instruction: "1. the instruction for the question goes here",
+        instruction: [{
+            inst1:"the instruction for the question goes here", 
+            inst2: "the instruction for the question goes here"
+        }],
         explanation:"the explanation 1 goes here",
         trial: 0
     },
     {
         id: 1002, 
         title: "Question Title 2", 
-        question: "2. the question goes here", 
+        question: "the question goes here", 
         options: [{
             optionA:"option a element",
             optionB:"option b element", 
             optionC:"option c element",
             optionD:"option d element "
         }],
-        answer: "2. the answer goes here", 
+        answer: "options.optionB", 
         level: 2, 
-        subject: "2. the question subject goes here", 
-        timeout: 60 ,icon: "image2.png", 
-        instruction: "2. the instruction for the question goes here", 
+        subject: "the question subject goes here", 
+        duration: 60 ,icon: "image2.png", 
+        instruction: [{
+            inst1:"the instruction for the question goes here", 
+            inst2: "the instruction for the question goes here"
+        }],
         explanation:"the explanation 2 goes here", 
         trial: 0 
     },
     {
         id: 1003, 
         title: "Question Title 3", 
-        question: "3. the question goes here", 
+        question: "the question goes here", 
         options: [{
             optionA:"option a element",
             optionB:"option b element", 
             optionC:"option c element",
             optionD:"option d element "
         }],
-        answer: "3. the answer goes here", 
+        answer: "options.optionC", 
         level: 2, 
-        subject: "3. the question subject goes here", 
-        timeout: 60 ,
+        subject: "the question subject goes here", 
+        duration: 60 ,
         icon: "image3.png", 
-        instruction: "3. the instruction for the question goes here", 
-        explanation:"the explanation 3 goes here", 
-        trial: 0 
+         instruction: [{
+            inst1:"the instruction for the question goes here", 
+            inst2: "the instruction for the question goes here"
+        }],
     },
     {
         id: 1004, 
         title: "Question Title 4", 
-        question: "4. the question goes here", 
+        question: "the question goes here", 
         options: [{
             optionA:"option a element",
             optionB:"option b element", 
             optionC:"option c element",
             optionD:"option d element "
         }],
-        answer: "4. the answer goes here", 
+        answer: "options.optionD", 
         level: 1,
-        subject: "3. the question subject goes here", 
-        timeout: 60 ,
+        subject: "the question subject goes here", 
+        duration: 60 ,
         icon: "image4.png", 
-        instruction: "4. the instruction for the question goes here", 
+         instruction: [{
+            inst1:"the instruction for the question goes here", 
+            inst2: "the instruction for the question goes here"
+        }], 
         explanation:"the explanation 4 goes here", 
         trial: 0 },
     {
         id: 1005, 
         title: "Question Title 5", 
-        question: "5. the question goes here", 
+        question: "the question goes here", 
         options: [{
             optionA:"option a element",
             optionB:"option b element", 
             optionC:"option c element",
             optionD:"option d element "
         }],
-        answer: "5. the answer goes here", 
+        answer: "options.optionA",  
         level: 3, 
-        subject: "2. the question subject goes here", 
-        timeout: 60, 
+        subject: "the question subject goes here", 
+        duration: 60, 
         icon: "image5.png", 
-        instruction: "5. the instruction for the question goes here", 
+         instruction: [{
+            inst1:"the instruction for the question goes here", 
+            inst2: "the instruction for the question goes here"
+        }],
         explanation:"the explanation 5 goes here", 
         trial: 0 },
     {
         id: 1006, 
         title: "Question Title 6", 
-        question: "6. the question goes here", 
+        question: "the question goes here", 
         options: [{
             optionA:"option a element",
             optionB:"option b element", 
             optionC:"option c element",
             optionD:"option d element "
         }],
-        answer: "6. the answer goes here", 
+        answer: "options.optionB", 
         level: 3,
-        subject: "1. the question subject goes here", 
-        timeout: 60 ,
+        subject: "the question subject goes here", 
+        duration: 60 ,
         icon: "image6.png", 
-        instruction: "6. the instruction for the question goes here", 
+         instruction: [{
+            inst1:"the instruction for the question goes here", 
+            inst2: "the instruction for the question goes here"
+        }],
         explanation:"the explanation 6 goes here", 
         trial: 0 },
 
@@ -128,16 +144,13 @@ var exams = [
 //answer
 //level
 //subject
-//timeout
+//duration
 //icon
 //instruction
 //explanation
 //trial
 
-
 //Routes goes here
-
-
 // getting all the datas
 router.get('/', function(req, res){
     res.json(exams);
@@ -169,7 +182,7 @@ router.post('/', function(req, res){
     if(!req.body.title ||
         !req.body.level.toString().match(/^[0-9]{1}$/g) ||
         !req.body.trial.toString().match(/^[0-9]{1}$/g) ||
-        !req.body.timeout.toString().match(/^[0-9]{2}$/g))
+        !req.body.duration.toString().match(/^[0-9]{2}$/g))
         {
         res.status(400);
         res.json({message: "Bad Request"});
@@ -183,7 +196,7 @@ router.post('/', function(req, res){
             answer: req.body.answer,
             level: req.body.level,
             subject: req.body.subject,
-            timeout: req.body.timeout,
+            duration: req.body.duration,
             icon: req.body.icon,
             instruction: req.body.instruction,
             explanation: req.body.explanation,
@@ -203,7 +216,7 @@ router.put('/:id', function(req, res){
 
       !req.body.level.toString().match(/^[0-9]{1}$/g) ||
        !req.body.trial.toString().match(/^[0-9]{1}$/g) ||
-       !req.body.timeout.toString().match(/^[0-9]{2}$/g) ||
+       !req.body.duration.toString().match(/^[0-9]{2}$/g) ||
        !req.params.id.toString().match(/^[0-9]{4,}$/g)
     )
       {
@@ -225,7 +238,7 @@ router.put('/:id', function(req, res){
                 answer: req.body.answer,
                 level: req.body.level,
                 subject: req.body.subject,
-                timeout: req.body.timeout,
+                duration: req.body.duration,
                 icon: req.body.icon,
                 instruction: req.body.instruction,
                 explanation: req.body.explanation,
@@ -244,7 +257,7 @@ router.put('/:id', function(req, res){
               answer: req.body.answer,
               level: req.body.level,
               subject: req.body.subject,
-              timeout: req.body.timeout,
+              duration: req.body.duration,
               icon: req.body.icon,
               instruction: req.body.instruction,
               explanation: req.body.explanation,
@@ -274,9 +287,4 @@ router.delete('/:id', function(req, res){
     }
 });
 //todo remove extra (unnecessary) newlines
-
-
-
-
-
 module.exports = router;
